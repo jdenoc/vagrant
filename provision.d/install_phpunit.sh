aa2d37fa4970b1b6ab5dc2c56e2ec9a297687732
jdenoc <jdenoc@gmail.com>
2015-09-21 16:09:47 -0500

    Cleaned up installations for phinx and phpunit

--- provision.d/install_phpunit.sh
+++ provision.d/install_phpunit.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 # Install PHPUnit
-COMPOSER_HOME="/home/vagrant"
 /usr/local/bin/composer global require "phpunit/phpunit=4.8.*"
-
-# update $PATH variable
-source $COMPOSER_HOME/.bash_profile
