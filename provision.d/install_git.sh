1acbbb9068b3a8730dbcaccc7f136ac7931c3cb7
jdenoc <jdenoc@gmail.com>
2015-09-21 16:13:28 -0500

    Cleaned up the provision.d/install_git.sh script.

--- provision.d/install_git.sh
+++ provision.d/install_git.sh
@@ -6,8 +6,9 @@ yum install -y git
 yum install -y gitflow
 
 git clone https://github.com/git/git.git /tmp/git
-cp /tmp/git/contrib/completion/git-completion.bash $USER_HOME/git-completion.bash
+cp /tmp/git/contrib/completion/git-completion.bash $USER_HOME/.git-completion.bash
 rm -rf /tmp/git
 
 echo "" >> $USER_HOME/.bash_profile
-echo "source ~/git-completion.bash" >> $USER_HOME/.bash_profile
+echo "# git auto-complete"
+echo "source ~/.git-completion.bash" >> $USER_HOME/.bash_profile
