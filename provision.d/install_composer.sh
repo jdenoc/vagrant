4173b107f1e42de49e614300abc6c6a43d771f8c
jdenoc <jdenoc@gmail.com>
2015-09-21 16:06:57 -0500

    Creating the /home/vagrant/.composer directory within the provision.d/install_composer.sh

--- provision.d/install_composer.sh
+++ provision.d/install_composer.sh
@@ -5,9 +5,7 @@ USER_HOME="/home/vagrant"
 # Install composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+mkdir -p $USER_HOME/.composer
 echo "" >> $USER_HOME/.bash_profile
 echo "# Add composer to PATH" >> $USER_HOME/.bash_profile
 echo 'export PATH='$USER_HOME'/.composer/vendor/bin:$PATH' >> $USER_HOME/.bash_profile
-
-# update $PATH variable
-source $USER_HOME/.bash_profile
