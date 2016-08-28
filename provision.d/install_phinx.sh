5a616819a9998c5428d4c47ae73fbfc067093443
jdenoc <jdenoc@gmail.com>
2016-08-27 20:59:36 -0500

    Removed the following provisioning scripts: * provision.d/install_elasticsearch.sh * provision.d/install_kibana.sh * provision.d/install_logstash.sh * provision.d/install_pear.sh * provision.d/install_phinx.sh * provision.d/install_phpunit.sh They're not or no longer used.

--- provision.d/install_phinx.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Phinx
-/usr/local/bin/composer global require robmorgan/phinx
-mkdir -p /usr/share/php/migrations
--- provision.d/install_phpunit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Install PHPUnit
-/usr/local/bin/composer global require "phpunit/phpunit=4.8.*"
